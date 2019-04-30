24e7e1ceede1e79fd3fa8fbc65c2d9255192c61e
GabriFili850 <gabriele.filigheddu@hotmail.com>
2019-04-30 11:15:42 +0100

    avoiding conditionals in render

--- src/index.js
+++ src/index.js
@@ -13,8 +13,7 @@ class App extends React.Component {
     );
   }
 
-  //React says we have to define render!!
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -24,7 +23,12 @@ class App extends React.Component {
     }
 
     return <Spinner message="Please accept location request" />;
-      }
+  }
+
+  //React says we have to define render!!
+    render() {
+      return <div className="border red">{this.renderContent()}</div>;
     }
+  }
 
 ReactDom.render(<App />, document.querySelector('#root'));
